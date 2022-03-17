370b0cedf350439c50e29c54f11e75ed36011c8a
kou.kikuyama <kou.kikuyama.ej@gmail.com>
2022-03-17 14:08:02 +0900

    a

---
+++ src/controllers/IndexServlet.java
@@ -0,0 +1,39 @@
+package controllers;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Message;
+import utils.DBUtil;
+@WebServlet("/index")
+public class IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public IndexServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    EntityManager em = DBUtil.createEntityManager();
+
+	    List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
+
+	    em.close();
+
+	    request.setAttribute("messages", messages);
+	    if(request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
+
+	    RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
+	    rd.forward(request, response);
+	}}
